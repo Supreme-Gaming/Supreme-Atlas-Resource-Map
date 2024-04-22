d6f0f608669408dcb40843783d15ca7f9fa3944c
Edgar Hernandez <Proantagonist@users.noreply.github.com>
2024-04-22 12:55:35 -0500

    Update map with Ino's dst (#3)

--- json/config.js
+++ json/config.js
@@ -1,4 +1,5 @@
-const config = {
+{
+	"AtlasMapServer": false,
 	"GPSBounds": {
 		"max": [
 			100,
@@ -11,9 +12,14 @@ const config = {
 	},
 	"GridOffset": 23333.333333333332,
 	"GridSize": 1400000,
+	"ItemLink": true,
+	"KofiLink": true,
 	"NodesPerAxis": 30,
+	"PathFinder": true,
+	"PinTool": true,
 	"ServersX": 7,
 	"ServersY": 7,
+	"WarehouseTool": false,
 	"XRange": 200,
 	"XScale": 1,
 	"YRange": 200,
